ccf40698dccb44b54f54a45df2dbb1c2fb5fc3dc
khusheegad <42761203+khusheegad@users.noreply.github.com>
2020-11-18 11:12:02 +0530

    q2wes
    lkm

---
+++ result.php
@@ -0,0 +1,20 @@
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+Welcome <?php echo $_GET["name"]; ?><br>
+Your email address is: <?php echo $_GET["email"]; ?>
+your address <?php echo $_GET["address"]; ?><br>
+Your email address is: <?php echo $_GET["browser"]; ?>
+Welcome <?php echo $_GET["dob"]; ?><br>
+Your email address is: <?php echo $_GET["econtact"]; ?>
+
+    
+</body>
+</html>
